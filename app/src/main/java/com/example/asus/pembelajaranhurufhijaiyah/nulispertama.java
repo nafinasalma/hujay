9f8a8845e6d0fff9579fa08d1b6168b8b70c5813
nafinasalma <nafinanfn@gmail.com>
2018-05-28 15:33:04 +0700

    R itu apasih?

--- app/src/main/java/com/example/asus/pembelajaranhurufhijaiyah/nulispertama.java
+++ app/src/main/java/com/example/asus/pembelajaranhurufhijaiyah/nulispertama.java
@@ -1,13 +1,73 @@
 package com.example.asus.pembelajaranhurufhijaiyah;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 
-public class nulispertama extends AppCompatActivity {
+public class nulispertama extends View {
+
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    public Button btnReset;
+    public LayoutParams params;
+
+    public nulispertama(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(6f);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+
+        btnReset = new Button(context);
+        btnReset.setText("Hapus");
+
+        params = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT);
+        btnReset.setLayoutParams(params);
+
+        btnReset.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0){
+                path.reset();
+                invalidate();
+            }
+        });
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nulispertama);
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
     }
 }
